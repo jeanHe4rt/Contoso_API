c74ad02ee5b24e136e3129e95871f31630ab8733
jeanHe4rt <jean0king@gmail.com>
2022-05-22 00:18:59 -0300

    Adicionado Get All e get das pizzas

---
+++ Controllers/PizzaController.cs
@@ -0,0 +1,41 @@
+﻿using Contoso_API.Models;
+using Contoso_API.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contoso_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        public PizzaController()
+        {
+
+        }
+
+        // GET all action
+        [HttpGet]
+        public ActionResult<List<Pizza>> GetAll() => PizzaService.GetAll();
+
+        // GET by Id action
+
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(string id)
+        {
+            var pizza = PizzaService.Get(id);
+            if(pizza == null)
+            {
+                return NotFound();
+            }
+
+            return pizza;
+        }
+
+
+        // POST action
+
+        // PUT action
+
+        // DELETE action
+    }
+}
